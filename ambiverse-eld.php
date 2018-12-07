d8c2c75af934ba1cd4fb130bb506903aaa5d485e
Dragan Milchevski <dragan.milcevski@gmail.com>
2018-12-07 09:55:15 +0100

    Changing licence to Apache.

--- ambiverse-eld.php
+++ ambiverse-eld.php
@@ -13,8 +13,8 @@
  * Version:           1.0
  * Author:            AmbiverseNLU
  * Author URI:        https://ambiversenlu.mpi-inf.mpg.de
- * License:           GPL-2.0+
- * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * License:           Apache 2.0
+ * License URI:       https://www.apache.org/licenses/LICENSE-2.0.html
  * Text Domain:       ambiverse-eld
  * Domain Path:       /languages
  */
